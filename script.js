b16cda6ff122b1c84402e5dbe748783318527645
Hedrian Dunn P. Alvarez <eh202201610@wmsu.edu.ph>
2025-02-28 10:32:40 +0800

    Dashboard

---
+++ script.js
@@ -0,0 +1,48 @@
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+
+allSideMenu.forEach(item=> {
+    const li = item.parentElement;
+
+    item.addEventListener('click', function(){
+        allSideMenu.forEach(i=> {
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
+});
+
+
+// TOGGLE SIDEBAR //
+
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sideBar = document.getElementById('sidebar');
+
+menuBar.addEventListener('click', function(){
+    sideBar.classList.toggle('hide');
+})
+// TOGGLE SIDEBAR //
+
+
+if(window.innerWidth <768){
+    sideBar.classList.add('hide');
+} else if(window.innerWidth < 576){
+    
+}
+
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+
+    searchButton.addEventListener('click', function(e){
+        if(window.innerWidth < 576){
+        e.preventDefault();
+        searchForm.classList.toggle('show');
+        if(searchForm.classList.contains('show')){
+            searchButtonIcon.classList.replace('bx-search', 'bx-x');
+        }
+    }
+})
+
+
+
+
